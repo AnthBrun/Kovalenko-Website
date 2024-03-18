0fcd63b0b77dec7b464e99a54d2b9ebdfbb5d3a5
AnthBrun <anthony.brunner004@stud.fh-dortmund.de>
2024-03-19 00:12:48 +0100

    second template commit ```

--- kovalenko-website/src/components/leistungen.jsx
+++ kovalenko-website/src/components/leistungen.jsx
@@ -3,13 +3,20 @@ import React, { Component } from 'react';
 class Leistungen extends Component {
     state = {  } 
     render() { 
-        return <div className="leistungen">
-            <img className="big" src="logo512.png" alt="logo"/>
-            <p>Ich lege Fliesen.</p>
-            <p>Ich mache Innenausbau.</p>
-            <p>Ich verbaue neue Duschen.</p>
-            <br />
-            <img className="little" src="logo192.png" alt="logo"/>
+        return <div className="leistungen-container">
+                <div className="big">
+                    <img className="big" src="logo512.png" alt="logo" />
+                </div>
+                <div className="leistungen-header">
+                    <p>Ich lege Fliesen.</p>
+                    <p>Ich mache Innenausbau.</p>
+                    <p>Ich verbaue neue Duschen.</p>
+                    <p>Ich verlege Laminat.</p>
+                </div>
+                <br />
+                <div className="little">
+                <img src="logo192.png" alt="logo"/>
+                </div>
             </div>;
     }
 }
--- kovalenko-website/src/components/navbar.jsx
+++ kovalenko-website/src/components/navbar.jsx
@@ -4,7 +4,7 @@ class Navbar extends Component {
     state = {  } 
     render() { 
         return <nav className="navbar navbar-expand-lg bg-body-tertiary">
-                <div className="container-fluid" style={{position: 'fixed', top: '1%', backgroundColor: 'white'}}>
+                <div className="container-fluid">
                     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
                         <span className="navbar-toggler-icon"></span>
                     </button>
